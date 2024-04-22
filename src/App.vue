c1a94e737e52c82d1af890e9ca0d8fd2d3f2d67a
Pilar Fernández Nieto <rla10309@educastur.es>
2024-04-22 23:16:45 +0200

    Listado, edición y borrado de pacientes

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,16 @@
 <script setup>
 import {ref, reactive} from "vue";
+
+import {uid} from "uid";
+
 import Header from "./components/Header.vue";
 import Formulario from "./components/Formulario.vue";
+import Paciente from "./components/Paciente.vue";
 
 const pacientes = ref([]);
 
 const paciente = reactive({
+  id: null,
   nombre: "",
   propietario: "",
   email: "",
@@ -13,6 +18,41 @@ const paciente = reactive({
   sintomas: ""
 });
 
+const guardarPaciente = () => {
+  if(paciente.id){
+   
+    const {id} = paciente;
+    const i = pacientes.value.findIndex(pacienteState => pacienteState.id === id );
+    pacientes.value[i] = {...paciente }
+  } else {
+    pacientes.value.push({ ...paciente, id: uid() });
+    
+  }
+  
+
+
+
+  // Reiniciar el objeto para vaciar el formulario
+  Object.assign(paciente, {
+    nombre: "",
+    propietario: "",
+    email: "",
+    alta: "",
+    sintomas: "",
+    id: null
+  });
+  
+}
+
+const actualizarPaciente = (id) => {
+  const pacienteEditar = pacientes.value.filter(paciente => paciente.id === id)[0]
+  Object.assign(paciente, pacienteEditar);
+  
+}
+const eliminarPaciente = (id) => {
+  pacientes.value = pacientes.value.filter(paciente => paciente.id !== id);
+ 
+}
 
 </script>
 
@@ -23,17 +63,21 @@ const paciente = reactive({
 
     <div class="mt-12 md:flex">
 
-      <Formulario 
-        v-model:nombre="paciente.nombre" 
-        v-model:propietario="paciente.propietario" 
-        v-model:email="paciente.email"
-        v-model:alta="paciente.alta" 
-        v-model:sintomas="paciente.sintomas" />
+      <Formulario v-model:nombre="paciente.nombre" v-model:propietario="paciente.propietario"
+        v-model:email="paciente.email" v-model:alta="paciente.alta" v-model:sintomas="paciente.sintomas" :id="paciente.id"
+        @guardar-paciente="guardarPaciente" />
 
       <div class="md:w-1/2 md:h-screen overflow-y-scroll">
         <h3 class="font-black text-3xl text-center">Administra tus pacientes</h3>
 
         <div v-if="pacientes.length > 0">
+
+          <p class="text-lg mt-5 text-center mb-10">Información de <span
+              class="text-indigo-600 font-bold">Pacientes</span></p>
+          <Paciente v-for="paciente in pacientes"
+            :paciente="paciente"
+            @actualizar-paciente="actualizarPaciente"
+            @eliminar-paciente = "eliminarPaciente" />
 
         </div>
         <div v-else class="mt-20 text-2xl text-center">No hay pacientes</div>
--- src/components/Formulario.vue
+++ src/components/Formulario.vue
@@ -1,30 +1,66 @@
 <script setup>
-    import {reactive} from "vue";
-    import Alerta from "./Alerta.vue";
+import { reactive, computed } from "vue";
+import Alerta from "./Alerta.vue";
 
-    const alerta = reactive({
-        tipo: "",
-        mensaje: ""
-    });
 
-    const validar = (e) => {
-      
-        if(Object.values(paciente).includes("")){
-           alerta.mensaje = "Todos los campos son obligatorios";
-           alerta.tipo = "error";
-           return;
-           
-        } 
-        console.log('Agregando');
+const alerta = reactive({
+    tipo: "",
+    mensaje: ""
+});
+
+const validar = (e) => {
+
+    if (Object.values(props).includes("")) {
+        alerta.mensaje = "Todos los campos son obligatorios";
+        alerta.tipo = "error";
+        return;
+
     }
+    emit("guardar-paciente");
+    alerta.mensaje = "Paciente almacenado correctamente";
+    alerta.tipo = "exito";
 
-    defineEmits(['update:nombre', 'update:propietario', 'update:email', 'update:alta', 'update:sintomas'])
-    const props = defineProps({
-        nombre: {
-            type: String,
-            required: true
-        }
-    })
+    setTimeout(() => {
+        Object.assign(alerta, {
+            tipo: "",
+            mensaje: ""
+        });
+    }, 3000)
+}
+
+const editando =computed(() => {
+    return props.id;
+})
+
+const emit = defineEmits(['update:nombre', 'update:propietario', 'update:email', 'update:alta', 'update:sintomas', 'guardar-paciente' ]);
+
+
+const props = defineProps({
+    nombre: {
+        type: String,
+        required: true
+    },
+    propietario: {
+        type: String,
+        required: true
+    },
+    email: {
+        type: String,
+        required: true
+    },
+    alta: {
+        type: String,
+        required: true
+    },
+    sintomas: {
+        type: String,
+        required: true
+    },
+    id: {
+        type: [String, null],
+        required: true
+    }
+})
 
 
 </script>
@@ -45,37 +81,37 @@
             <div class="mb-5">
                 <label class="text-gray-700 uppercase font-bold" for="mascota">Nombre Mascota</label>
                 <input type="text" id="mascota" class="border-2 w-full p-2 mt-2 placeholder-gray-400 rounded-md"
-                    placeholder="Nombre de la mascota" @input="$emit('update:nombre', $event.target.value)">
+                    placeholder="Nombre de la mascota" :value="nombre"
+                    @input="$emit('update:nombre', $event.target.value)">
             </div>
             <div class="mb-5">
                 <label class="text-gray-700 uppercase font-bold" for="propietario">Nombre Propietario</label>
                 <input type="text" id="propietario" class="border-2 w-full p-2 mt-2 placeholder-gray-400 rounded-md"
-                    placeholder="Nombre del propietario" @input="$emit('update:propietario', $event.target.value)">
+                    placeholder="Nombre del propietario" :value="propietario"
+                    @input="$emit('update:propietario', $event.target.value)">
             </div>
             <div class="mb-5">
                 <label class="text-gray-700 uppercase font-bold" for="email">Email</label>
                 <input type="email" id="email" class="border-2 w-full p-2 mt-2 placeholder-gray-400 rounded-md"
-                    placeholder="Email del propietario" @input="$emit('update:email', $event.target.value)">
+                    placeholder="Email del propietario" :value="email"
+                    @input="$emit('update:email', $event.target.value)">
             </div>
             <div class="mb-5">
                 <label class="text-gray-700 uppercase font-bold" for="alta">Alta</label>
-                <input type="date" id="alta" class="border-2 w-full p-2 mt-2 rounded-md"
+                <input type="date" id="alta" class="border-2 w-full p-2 mt-2 rounded-md" :value="alta"
                     @input="$emit('update:alta', $event.target.value)">
             </div>
             <div class="mb-5">
                 <label class="text-gray-700 uppercase font-bold" for="sintomas">Síntomas</label>
                 <textarea id="sintomas" placeholder="Describe los síntomas"
-                    class="border-2 w-full p-2 mt-2 placeholder-gray-400 rounded-md h-40"
+                    class="border-2 w-full p-2 mt-2 placeholder-gray-400 rounded-md h-40" :value="sintomas"
                     @input="$emit('update:sintomas', $event.target.value)"></textarea>
 
             </div>
             <input type="submit"
                 class="bg-indigo-600 w-full p-3 text-white uppercase font-bold hover:bg-indigo-700 cursor-pointer transition-colors"
-                value="Registrar Paciente">
+                :value="[editando ? 'Guardar Cambios' : 'Registar Paciente' ]">
 
         </form>
     </div>
 </template>
-
-
-
