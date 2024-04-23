4620494d9700e282f48cb941cbd9c65898b76b9b
Pilar Fernández Nieto <rla10309@educastur.es>
2024-04-23 16:05:49 +0200

    Proyecto finalizado

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <script setup>
-import {ref, reactive} from "vue";
+import { ref, reactive, onMounted, watch } from "vue";
 
-import {uid} from "uid";
+import { uid } from "uid";
 
 import Header from "./components/Header.vue";
 import Formulario from "./components/Formulario.vue";
@@ -18,19 +18,34 @@ const paciente = reactive({
   sintomas: ""
 });
 
+watch(pacientes, () => {
+  guardarPacientesLocalStorage();
+}, {
+  deep: true
+});
+
+onMounted(() => {
+  const pacientesStorage = localStorage.getItem("pacientes");
+  if(pacientesStorage){
+    pacientes.value = JSON.parse(pacientesStorage);
+  }
+})
+
+const guardarPacientesLocalStorage = () => {
+  localStorage.setItem("pacientes", JSON.stringify(pacientes.value));
+}
+
 const guardarPaciente = () => {
-  if(paciente.id){
-   
-    const {id} = paciente;
-    const i = pacientes.value.findIndex(pacienteState => pacienteState.id === id );
-    pacientes.value[i] = {...paciente }
+  if (paciente.id) {
+
+    const { id } = paciente; // Destructuring
+
+    const i = pacientes.value.findIndex(pacienteState => pacienteState.id === id);
+    pacientes.value[i] = { ...paciente }
   } else {
     pacientes.value.push({ ...paciente, id: uid() });
-    
+
   }
-  
-
-
 
   // Reiniciar el objeto para vaciar el formulario
   Object.assign(paciente, {
@@ -41,17 +56,17 @@ const guardarPaciente = () => {
     sintomas: "",
     id: null
   });
-  
+
 }
 
 const actualizarPaciente = (id) => {
   const pacienteEditar = pacientes.value.filter(paciente => paciente.id === id)[0]
   Object.assign(paciente, pacienteEditar);
-  
+
 }
 const eliminarPaciente = (id) => {
   pacientes.value = pacientes.value.filter(paciente => paciente.id !== id);
- 
+
 }
 
 </script>
@@ -64,8 +79,8 @@ const eliminarPaciente = (id) => {
     <div class="mt-12 md:flex">
 
       <Formulario v-model:nombre="paciente.nombre" v-model:propietario="paciente.propietario"
-        v-model:email="paciente.email" v-model:alta="paciente.alta" v-model:sintomas="paciente.sintomas" :id="paciente.id"
-        @guardar-paciente="guardarPaciente" />
+        v-model:email="paciente.email" v-model:alta="paciente.alta" v-model:sintomas="paciente.sintomas"
+        :id="paciente.id" @guardar-paciente="guardarPaciente" />
 
       <div class="md:w-1/2 md:h-screen overflow-y-scroll">
         <h3 class="font-black text-3xl text-center">Administra tus pacientes</h3>
@@ -74,10 +89,8 @@ const eliminarPaciente = (id) => {
 
           <p class="text-lg mt-5 text-center mb-10">Información de <span
               class="text-indigo-600 font-bold">Pacientes</span></p>
-          <Paciente v-for="paciente in pacientes"
-            :paciente="paciente"
-            @actualizar-paciente="actualizarPaciente"
-            @eliminar-paciente = "eliminarPaciente" />
+          <Paciente v-for="paciente in pacientes" :paciente="paciente"            @actualizar-paciente="actualizarPaciente"
+          @eliminar-paciente="eliminarPaciente" />
 
         </div>
         <div v-else class="mt-20 text-2xl text-center">No hay pacientes</div>
@@ -87,5 +100,3 @@ const eliminarPaciente = (id) => {
     </div>
   </div>
 </template>
-
-
